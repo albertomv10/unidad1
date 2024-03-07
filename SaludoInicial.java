e96ccd937b481632e70102c8f07ea2b16fec41b4
Alberto Medina <amedina10@alu.ucam.edu>
2024-03-07 17:56:58 +0100

    vamooo

---
+++ SaludoInicial.java
@@ -0,0 +1,27 @@
+package unidad1;
+
+/**
+ * 
+ * @author 309083
+ *
+ */
+
+public class SaludoInicial {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Saludo Inicial");
+		
+		double a=2.50;
+		double b=3.20;
+		double c= a+b;
+		System.out.print("a+b=");
+		System.out.print(c);
+		
+		int ano;
+		final String SALUDO;
+		
+
+	}
+
+}
